ec8ad2e3e6c2d77a0be33c67678a18b1572e7c92
DmytroBozhor <[dimaastop1@gmail.com]>
2023-04-10 15:19:19 +0200

    finished project

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Car {
+
+    public static void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private static void startFuelSystem() {
+    }
+
+    private static void startCommand() {
+    }
+
+    private static void startElectricity() {
+    }
+
+}
